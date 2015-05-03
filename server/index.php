4be48c064c11b821822c1a9dfb3a17504a8f22ab
BestPig <bestpig82@gmail.com>
2015-05-03 20:22:12 +0200

    Add first release of client and server files

---
+++ server/index.php
@@ -0,0 +1,63 @@
+<?php
+/* Created by BestPig */
+
+define('DATAPATH', './data/');
+
+require 'storage.php';
+
+$db = new Storage(DATAPATH.'/files.sqlite', DATAPATH);
+
+if (isset($_GET['request'])) {
+	if ($_GET['request'] == "upload" && !empty($_POST['tag']) && !empty($_FILES['file']['name'])) {
+		$id = $db->addFile($_POST['tag'], $_FILES['file']['name'].".tar.gz", 0);
+		if ($id !== NULL) {
+			$filepath = DATAPATH."/".$id.".bin";
+			if (move_uploaded_file($_FILES['file']['tmp_name'], $filepath) && file_exists($filepath)) {
+				echo json_encode(array('status' => 'OK', 'tag'=>$_POST['tag']));
+				exit();
+			}
+		}
+	}
+	if ($_GET['request'] == "list") {
+		echo json_encode($db->getAllFiles());
+	}
+
+	if ($_GET['request'] == 'get' && !empty($_GET['tag'])) {
+		$file = $db->getFile($_GET['tag']);
+		$filepath = DATAPATH."/".$file[0]['id'].".bin";
+		if (!empty($file)) {
+			header("Content-Disposition: attachment; filename=" . urlencode($file[0]['name']));    
+			header("Content-Type: application/force-download");
+			header("Content-Type: application/octet-stream");
+			header("Content-Type: application/download");
+			header("Content-Description: File Transfer");             
+			header("Content-Length: " . filesize($filepath));
+			readfile($filepath);
+		}
+		else {
+			header('HTTP/1.0 404 Not Found');
+			echo "File not found.";
+		}
+	}
+
+	if ($_GET['request'] == 'delete' && !empty($_GET['tag'])) {
+		$ret = $db->getFile($_GET['tag']);
+		if (!empty($ret)) {
+			$db->deleteFileTag($_GET['tag']);
+			echo json_encode(array('status'=>"OK"));
+		}
+		else {
+			echo json_encode(array('status'=>"Tag not existing"));
+		}
+	}
+
+	if ($_GET['request'] == 'reset') {
+		$files = $db->getAllFiles();
+		$i = 0;
+		foreach ($files as $file) {
+			$db->deleteFileId($file['id']);
+			++$i;
+		}
+		echo json_encode(array('status'=>"OK", 'files_deleted'=>$i));
+	}
+}
---
+++ server/storage.php
@@ -0,0 +1,67 @@
+<?php
+/* Created by BestPig */
+
+class Storage {
+	private $_db;
+	private $_dataPath;
+
+	public function __construct($sqliteFile, $dataPath) {
+		$this->_dataPath = $dataPath;
+
+		$this->_db = new PDO('sqlite:'.$sqliteFile);
+
+		$this->_db->exec("CREATE TABLE IF NOT EXISTS files (
+			id INTEGER PRIMARY KEY,
+			name TEXT,
+			tag TEXT,
+			compressed INTEGER)");
+	}
+
+	public function getFile($tag) {
+		$sql = "SELECT id, name FROM files WHERE tag = :tag";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->bindParam(':tag', $tag);
+		$stmt->execute();
+
+		$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $res;
+	}
+
+	public function getAllFiles() {
+		$sql = "SELECT id, name, tag FROM files";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->execute();
+
+		$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $res;
+	}
+
+	public function deleteFileId($id) {
+		$sql = "DELETE FROM files WHERE id = :id";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->bindParam(':id', $id);
+		$stmt->execute();
+        @unlink($this->_dataPath.'/'.$id.'.bin');
+	}
+
+	public function deleteFileTag($tag) {
+		$exists = $this->getFile($tag);
+		foreach ($exists as $file) {
+			$this->deleteFileId($file['id']);
+		}
+	}
+
+	public function addFile($tag, $name, $compressed) {
+		$exists = $this->getFile($tag);
+		foreach ($exists as $file) {
+			$this->deleteFileId($file['id']);
+		}
+		$sql = "INSERT INTO files (name, tag, compressed) VALUES (:name, :tag, :compressed)";
+		$stmt = $this->_db->prepare($sql);
+		$stmt->bindParam(':tag', $tag);
+		$stmt->bindParam(':name', $name);
+		$stmt->bindParam(':compressed', $compressed);
+		$stmt->execute();
+		return $this->_db->lastInsertId();
+	}
+}
